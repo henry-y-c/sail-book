adcfa3424e544e612d2d802848d0fadf551c4f92
Henry Chen <yankang.henry.chen@gmail.com>
2018-02-03 16:36:06 -0800

    Change base layer to detailed caribbean map & fixed missing key issue

--- src/components/WaypointsMapLayer.jsx
+++ src/components/WaypointsMapLayer.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 export default class WaypointsMapLayer extends React.Component {
   render() {
+    var keyId=0
     const waypoints = this.props.waypoints.map(waypoint => 
-      <circle cx={waypoint.N + 180} cy={90 - waypoint.L} r="0.02" fill="blue" />
+      <circle
+        key={`waypoint_${keyId++}`}
+        cx={waypoint.N + 180}
+        cy={90 - waypoint.L}
+        r="0.02"
+        fill="blue"
+      />
     );
     return (
       <g id="WaypointsLayer" fill="none" fillRule="nonzero">
